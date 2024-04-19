f07589544418c3cd09fbb6c4d4266f91729db6ed
Md. Easin <mdeasintuha@gmail.com>
2024-04-19 21:49:28 +0600

    day 1

--- week 5/day 4/NASA.cpp
+++ week 5/day 4/NASA.cpp
@@ -1,12 +1,24 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b,c=0,d=0;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++)
     {
-        
+        scanf("%d",&b);
+        if(b>0)
+        {
+            c=b+c;
+        }
+        else
+        {
+            d=b+d;
+        }
     }
+    printf("%d %d\n",c,d);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
